d86ae8b336f33a4bc3832251669d270908211f83
Sohail Nadaf <sohailnadaf967@gmail.com>
2024-05-15 17:17:36 +0530

    firebase registered app on the console and configured in our react-app

---
+++ src/config/firebase.config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYLyja0r21fUHXc16p2eDTphgBsOjyHn4",
+  authDomain: "fir-backend-c2b2d.firebaseapp.com",
+  projectId: "fir-backend-c2b2d",
+  storageBucket: "fir-backend-c2b2d.appspot.com",
+  messagingSenderId: "261917479732",
+  appId: "1:261917479732:web:a7fd19a6d42ee2b4cecb61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
